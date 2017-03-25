ac692a189711330f97b91c574161b6085137dadd
Delson <delsonchemplani@gmail.com>
2017-03-25 18:24:20 +0530

    Admin page insertion

--- app/admin/admin.js
+++ app/admin/admin.js
@@ -9,6 +9,22 @@
 .controller('AdminCtrl', ['$scope',function($scope) {
 
   
+$scope.pushItem = function(event) {
+  event.preventDefault();  
+   
+     
+    console.log($scope.item)
+  
+   var itemId=$scope.item.itemCode;
+  // var newItems = new Firebase('https://kalahullu-2064b.firebaseio.com/items');
+  firebase.database().ref('items/' + itemId).set($scope.item);
+   //newItems.push(item);
+   console.log('inserted');
+  
+  
+
+
+}
 
 
 
