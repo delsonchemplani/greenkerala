076cb7d51aaaab2ed72c726712326a9fa26dd2ce
Delson <delsonchemplani@gmail.com>
2017-03-26 14:44:24 +0530

    image upload

--- app/admin/admin.js
+++ app/admin/admin.js
@@ -11,11 +11,35 @@
   
 $scope.pushItem = function(event) {
   event.preventDefault();  
+  var itemId=$scope.item.itemCode;
    
-     
-    console.log($scope.item)
+       //  var sFileName = $("#imageFile").val();
+    
+   
+  var preview = document.querySelector('img'); 
+var file    = document.getElementById('imageFile').files[0];
+ var storageRef = firebase.storage().ref(itemId+'/itemImages/'+file.name);
+console.log('myFile'+file.name);
+  var reader  = new FileReader();
+
+  reader.addEventListener("load", function () {
+    preview.src = reader.result;
+  }, false);
+
+
+  if (file) {
+    //reader.readAsDataURL(file);
+    var uploadTask= storageRef.put(file).then(function(snapshot) {
+    console.log('Uploaded a blob or file!'+snapshot.downloadURL);
+    $scope.item.images={};
+    $scope.item.images[0]=snapshot.downloadURL;
+    });
+  }
+  console.log(file);
+
+  //  console.log(sFileName)
   
-   var itemId=$scope.item.itemCode;
+   
   // var newItems = new Firebase('https://kalahullu-2064b.firebaseio.com/items');
   firebase.database().ref('items/' + itemId).set($scope.item);
    //newItems.push(item);
