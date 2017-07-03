2c8b41ede467db12a8e5f5d7255f754745e4c036
Delson <delsonchemplani@gmail.com>
2017-07-03 21:27:21 +0530

    paytm

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
-var express=require('express');
-var path=require('path');
-var bodyParser=require('body-parser');
-var index=require('./app/app');
+var http = require('http');
+var url  = require('url');
 
-var port=3000;
-var app=express();
+function start(route){	
+	var onRequest = function(request,response){
+		route(request,response);		
+	};
+	http.createServer(onRequest).listen(3000);
+	console.log("Server has started");
+}
 
-//View engine
-
-
-  app.get('*', function(req, res) {
-        res.sendfile('./app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
-
-/*app.get('/', function(req, res){
-  // res.send("Hello World!");
-  next();
-});*/
-
-app.listen(3000);
+exports.start= start;
