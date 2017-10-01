e758de0073fc95fc591f3318d2e2fb0dc6b3b982
Delson <delsonchemplani@gmail.com>
2017-10-01 15:13:46 +0530

    clean-up:

--- app/chout/checkout.js
+++ app/chout/checkout.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
  
 
  
@@ -9,21 +9,7 @@
 app.controller('CheckoutCtrl', ['$scope','FirebaseService',function($scope,FirebaseService) {
 
 
-var options = { "key": "YOUR_KEY_ID", 
-				"amount": "2000", // 2000 paise = INR 20 
-				"name": "Merchant Name", 
-				"description": "Purchase Description", 
-				"image": "/your_logo.png", 
-				"handler": function (response){ alert(response.razorpay_payment_id); }, 
-				"prefill": { "name": "Harshil Mathur", 
-				"email": "harshil@razorpay.com" }, 
-				"notes": { "address": "Hello World" }, 
-				"theme": { "color": "#F37254" } 
-			}; 
-var rzp1 = new Razorpay(options); 
-document.getElementById('rzp-button1').onclick = function(e){
-	alert('hi');
-		 rzp1.open(); e.preventDefault(); } 
+
 
  //future method
  $scope.getOrderItems=function(){
@@ -40,4 +26,4 @@ document.getElementById('rzp-button1').onclick = function(e){
     });
   };
 
-}]);
+}]);*/
